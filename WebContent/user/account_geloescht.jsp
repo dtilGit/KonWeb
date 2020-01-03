6ae8763ba1a046dfebb8b27e7eb2968b7a0d2c85
VeroGit <nika12460@yandex.ru>
2020-01-03 12:29:48 +0100

    add deleteaccount

---
+++ WebContent/user/account_geloescht.jsp
@@ -0,0 +1,21 @@
+<%--Veronika Tschemodanov --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<base href="${pageContext.request.requestURI}" />
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
+
+<title>Account gelöscht</title>
+</head>
+<body>
+<%@ include file="../jspf/header.jspf"%>
+
+<p> Ihr Account wurde erfogreich gelöscht. </p>
+
+<%@ include file="../jspf/footer.jspf"%>
+</body>
+</html>
---
+++ src/servlets/AccountLoeschen.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import beans.ArtikelBean;
+import beans.RegistrBean;
+
+/**
+ * Servlet implementation class kategorie_loeschen
+ */
+@WebServlet("/AccountLoeschen")
+public class AccountLoeschen extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Resource(lookup = "java:jboss/datasources/MySqlThidbDS")
+	private DataSource ds;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+
+		HttpSession session = request.getSession();
+		RegistrBean kunde = (RegistrBean) session.getAttribute("login");
+
+		int kunden_id = kunde.getId();
+
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("DELETE * FROM thidb.kunde WHERE kunde_id = ?")) {
+			pstmt.setInt(1, kunden_id);
+			pstmt.executeUpdate();
+		}
+
+		catch (Exception ex) {
+			throw new ServletException(ex.getMessage());
+		}
+
+		request.getSession().invalidate();
+		
+		final RequestDispatcher dispatcher = request.getRequestDispatcher("user/artikel_geloescht.jsp");
+		dispatcher.forward(request, response);
+	}
+}
