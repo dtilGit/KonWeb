4befb5527d65739297f9f1bab48d081d74011fc2
hdave <david-haeusler@gmx.de>
2020-01-09 23:35:24 +0100

    email check

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
 	var datenform = document.getElementById("regform");
 	datenform.addEventListener("submit", pruefereg);
+	datenform.addEventListener("submit", checkemail);
 }
 
 // hier wird das Register Form auf Eingaben geprüft
@@ -11,20 +12,19 @@ function pruefereg(event) {
 	var nachname = document.getElementById("nachnamereg").value;
 	var korrekt = document.getElementById("pruefnn");
 	var vorname = document.getElementById("vornamereg").value;
-	var email = document.getElementById("emailreg").value;
 	var pw = document.getElementById("pwreg").value;
 	var strasse = document.getElementById("strassereg").value;
 	var hausnummer = document.getElementById("hausnrreg").value;
 	var postleitzahl = document.getElementById("plzreg").value;
 	var ort = document.getElementById("ortreg").value;
 	var land = document.getElementById("landreg").value;
-	var onlyletters = /^[A-Za-zß-ü\s]+$/;
-	
+	//var onlyletters = /^[A-Za-zß-ü\s]+$/;
+
 	var sonderzeichen = /\W+/;
 	var zahlen = /\d/;
 	var keinebuchstaben = /\D/;
-	//Bedingung für Mail
-	var emailzeichen = /^([\w.+-]+@[\w.-]+\.[a-zA-Z]{2,3})$/;
+	// Bedingung für Mail
+//	var emailzeichen = /^([\w.+-]+@[\w.-]+\.[a-zA-Z]{2,3})$/;
 
 	if (nachname.match(zahlen)) {
 		alert("Der Nachname darf keine Zahlen enthalten.");
@@ -42,12 +42,7 @@ function pruefereg(event) {
 		alert("Der Vorname darf keine Sonderzeichen enthalten.");
 		event.preventDefault();
 		return;
-	} else if (email.match(emailzeichen)) {
-		alert("Die Email hat kein korrektes Format. Vor dem @ darf kein Sonderzeichen außer . - + stehen! Genauso wie hinter dem @! Zusätzlich muss ein . in deinem Domäne Part existieren!");
-		event.preventDefault();
-		return;
-	} 
-	else if (strasse.match(zahlen)) {
+	} else if (strasse.match(zahlen)) {
 		alert("Die Strasse darf keine Zahlen enthalten.");
 		event.preventDefault();
 		return;
@@ -56,11 +51,16 @@ function pruefereg(event) {
 		event.preventDefault();
 		return;
 	} else if (hausnummer.match(sonderzeichen)) {
-		alert("Die Hausnummer darf keine Sonderzeichen enthalten, nur Buchstaben und Zahlen.");
+		alert("Die Hausnummer muss eine Zahl enthalten! Buchstaben sind erlaubt. Zudem darf es keine Sonderzeichen enthalten! ");
 		event.preventDefault();
 		return;
-	} else if (postleitzahl.match(keinebuchstaben) || postleitzahl.match(sonderzeichen) ) {
-		alert("Die Postleitzahl darf nur Zahlen enthalten.");
+	} else if (postleitzahl.match(keinebuchstaben)
+			|| postleitzahl.match(sonderzeichen)) {
+		alert("Die Postleitzahl darf nur Zahlen enthalten und davon 5, um eine vollständige PLZ zu erhalten!");
+		event.preventDefault();
+		return;
+	} else if (postleitzahl.length !=5) {
+		alert("Die Postleitzahl muss 5 Zahlen enthalten!");
 		event.preventDefault();
 		return;
 	} else if (ort.match(zahlen)) {
@@ -82,19 +82,25 @@ function pruefereg(event) {
 	}
 
 	else {
-		alert( "Sie haben ihre Daten erfolgreich eingepflegt!");
+//		alert("Sie haben ihre Daten erfolgreich eingepflegt!");
 	}
 
-	//	if (nachname.match(sonderzeichen)) {
-	//		throw "Nachname gültig!"
-	//		korrekt.innerhtml = "Nachname gültig!";
-	//	} else {
-	//
-	//		alert("Der Nachname darf keine Zahlen oder Sonderzeichen entahlten.");
-	//		event.preventDefault();
-	//		return;
-	//	}
 }
+
+
+function checkemail(event){
+	var email = document.getElementById("emailreg").value;
+	var emailzeichen = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	
+	if(email.match(emailzeichen)){
+		alert("Sie haben ihre Daten erfolgreich eingepflegt!");
+	}else{
+		alert("Die Email hat kein korrektes Format. Vor dem @ darf kein Sonderzeichen außer . - + stehen! Genauso wie hinter dem @! Zusätzlich muss ein . in deinem Domäne Part existieren!");
+		event.preventDefault();
+		return;
+	}
+}
+
 // document.addEventListener("DOMContentLoaded", pruefedaten);
 //
 // function init(){
@@ -123,93 +129,4 @@ function pruefereg(event) {
 // }
 //
 // }
-
-// Überprüfung für das Formular bei Bearbeitung des Profils
-// function pruefedaten(){
-// var nachname = document.getElementById("nachname").value;
-// var vorname = document.getElementById("vorname").value;
-// var strasse = document.getElementById("strasse").value;
-// var hausnummer = document.getElementById("hausnummer").value;
-// var postleitzahl = document.getElementById("postleitzahl").value;
-// var ort = document.getElementById("ort").value;
-// var land = document.getElementById("land").value;
-// var sonderzeichen =
-//
-// if (nachname.trim() === "") {
-// alert("Sie müssen einen Nachnamen eintragen!");
-// } else if (vorname.trim() === "") {
-// alert("Sie müssen einen Vornamen eintragen!");
-// } else if (strasse.trim() === "") {
-// alert("Sie müssen einen Strassennamen eintragen!");
-// } else if (hausnummer.trim() === "") {
-// alert("Sie müssen eine Hausnummer eintragen!");
-// } else if (postleitzahl.trim() === "") {
-// alert("Sie müssen eine PLZ eintragen!");
-// } else if (ort.trim() === "") {
-// alert("Sie müssen einen Ort eintragen!");
-// } else if (land.trim() === "") {
-// alert("Sie müssen ein Land eintragen!");
-// } else{
-//		
-// }
-// }
-
-// else if (vorname.trim() === "") {
-// throw "Sie müssen einen Vornamen eintragen!";
-// } else if (email.trim() === "") {
-// throw "Sie müssen eine Email eintragen!";
-// } else if (pw.trim() === "") {
-// throw "Sie müssen ein Passwort eintragen!";
-// } else if (strasse.trim() === "") {
-// throw "Sie müssen einen Strassennamen eintragen!";
-// } else if (hausnummer.trim() === "") {
-// throw "Sie müssen eine Hausnummer eintragen!";
-// } else if (postleitzahl.trim() === "") {
-// throw "Sie müssen eine PLZ eintragen!";
-// } else if (ort.trim() === "") {
-// throw "Sie müssen einen Ort eintragen!";
-// } else if (land.trim() === "") {
-// throw "Sie müssen ein Land eintragen!";
-//		
-// }
-
-// lediglich alerts werden geworfen, wenn die Felder leer sind - jedoch
-// überprüft diese Funktion eigentlich required automatisch
-// try {
-// if (nachname.trim() === "") {
-// throw "Sie müssen einen Nachnamen eintragen!";
-// } else if (vorname.trim() === "") {
-// throw "Sie müssen einen Vornamen eintragen!";
-// } else if (email.trim() === "") {
-// throw "Sie müssen eine Email eintragen!";
-// } else if (pw.trim() === "") {
-// throw "Sie müssen ein Passwort eintragen!";
-// } else if (strasse.trim() === "") {
-// throw "Sie müssen einen Strassennamen eintragen!";
-// } else if (hausnummer.trim() === "") {
-// throw "Sie müssen eine Hausnummer eintragen!";
-// } else if (postleitzahl.trim() === "") {
-// throw "Sie müssen eine PLZ eintragen!";
-// } else if (ort.trim() === "") {
-// throw "Sie müssen einen Ort eintragen!";
-// } else if (land.trim() === "") {
-// throw "Sie müssen ein Land eintragen!";
-//		
-// } else {
-// if(nachname.match (/\d/)){
-// throw "Der Nachname darf keine Zahlen entahlten.";
-// } else if (nachname.match (/\W+/)){
-// throw "Der Nachname darf keine Sonderzeichen entahlten." ;
-//			
-// } else if (vorname.match (/\d/)) {
-// throw "Der Vorname darf keine Zahlen entahlten.";
-// } else if (vorname.match (/\W+/)){
-// throw "Der Nachname darf keine Sonderzeichen entahlten.";
-// }
-// }
-// } catch (e){
-// // message.innerHTML = "Es ist etwas schief gelaufen!" + e.message;
-////		e.printStackTrace();
-//		alert("Es ist etwas schief gelaufen!");
-//	}
 
