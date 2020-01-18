1ed79eea673ce40f339a6d7d1fe0a449c2c55276
VeroGit <nika12460@yandex.ru>
2020-01-18 20:51:52 +0100

    login weiterleitung

--- WebContent/js/checkregform.js
+++ WebContent/js/checkregform.js
@@ -134,33 +134,3 @@ function formatPruefen(event){
 	}
 }
 
-
-// document.addEventListener("DOMContentLoaded", pruefedaten);
-//
-// function init(){
-// var nachname1 = document.getElementById("nachname");
-// // The focus() method is used to give focus to an element (if it can be
-// focused).
-// //
-// // Tip: Use the blur() method to remove focus from an element.
-// nachname1.focus();
-// nachname1.addEventListener("blur", zeichenCheck);
-//	
-// }
-// https://wiki.selfhtml.org/wiki/JavaScript/DOM/Event/blur --> Erklärung
-// für
-// die Blur und focus
-
-// function zeichenCheck(){
-// var nachname2 = document.getElementById("nachname").value;
-// var ausgabe = document.getElementById("nachname2");
-// var zeichen = /^[A-Za-z\s]+$/;
-// if(nachname2.match(zeichen)){
-// ausgabe.innerHTML = "passt";
-// }else {
-// ausgabe.innerHTML="Bitte nur Buchstaben";
-// // event.preventDefault();
-// }
-//
-// }
-
